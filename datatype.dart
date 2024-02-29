85b52a1be52541c25bf09931a761de660474d3a2
9471 <elkananyachae255@gmail.com>
2024-02-29 10:44:57 +0300

    first commit

---
+++ datatype.dart
@@ -0,0 +1,32 @@
+void main() {
+  // Integer data type
+  int age = 25; // Example of an integer variable storing age
+
+  // Double data type
+  double height = 5.9; // Example of a double variable storing height
+
+  // String data type
+  String name = "John"; // Example of a string variable storing name
+
+  // List data type
+  List<String> colors = [
+    'Red',
+    'Green',
+    'Blue'
+  ]; // Example of a list of strings
+
+  // Map data type
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'height': 5.6,
+    'isStudent': true,
+  }; // Example of a map containing person's information
+
+  // Printing out the values
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Colors: $colors");
+  print("Person: $person");
+}
